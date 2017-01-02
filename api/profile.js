4c1bf393fa8202593f5b0163405457368da8f898
Scott Sayles <scott.sayles@gmail.com>
2017-01-02 17:55:15 -0500

    Connect to elasticache/redis.

--- api/profile.js
+++ api/profile.js
@@ -1,16 +1,41 @@
 'use strict';
 
 const path = require('path');
+const redis = require('redis');
+const Bluebird = require('bluebird');
 
 module.exports.handler = (event, context, callback) => {
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: path.basename(__filename),
-        input: event,
-      }),
-  };
+	// todo: client creation and config to lib
+	// consider disposer pattern
+	// configure retry/timeout logic
+	const client = redis.createClient({
+		host: 'redis.nkysj7.ng.0001.use1.cache.amazonaws.com'
+	});
 
-  callback(null, response);
+	Bluebird.promisifyAll(client);
+
+	const response = {
+		statusCode: 200,
+		body: '{}'
+	};
+
+	let err;
+
+	client.hgetallAsync('1').then(val => {
+		console.log('val', val);
+		response.body = JSON.stringify({message: val + ''});
+	})
+	.catch(e => {
+		response.statusCode = 500;
+		response.body = JSON.stringify({message: e.message || e.toString()});
+		err = e;
+	})
+	.finally(() => {
+		try {
+			client.quit();
+		} catch (e) {
+		}
+		callback(err, response);
+	});
 
 };
